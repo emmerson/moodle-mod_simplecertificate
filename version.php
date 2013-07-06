ba757b8836d4b4082ef2f07372b11974da4a50fd
Carlos Alexandre S. da Fonseca <bozohhot@hotmail.com>
2013-07-06 01:40:03 -0300

    v.2.0.2

--- version.php
+++ version.php
@@ -9,9 +9,10 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or late
  */
 
-$module->version  = 2013060501; // The current module version (Date: YYYYMMDDXX)
+$module->version  = 2013070501; // The current module version (Date: YYYYMMDDXX)
 $module->requires = 2012062500;  // Requires this Moodle version (moodle 2.3.x)
 $module->cron     = 4 * 3600;    // Period for cron to check this module (secs)
 $plugin->component = 'mod_simplecertificate';
 $module->release  = '2.0.2';       // Human-friendly version name
+//MATURITY_ALPHA, MATURITY_BETA, MATURITY_RC, MATURITY_STABLE
 $module->maturity = MATURITY_STABLE;
